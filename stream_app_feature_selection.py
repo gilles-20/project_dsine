c312dce2f966967ef37967ab021cee6743df90f2
gilles-20 <dongmo20gilles@gmail.com>
2024-12-02 15:39:14 +0100

    added an additional tab for prediction

--- stream_app_feature_selection.py
+++ stream_app_feature_selection.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import numpy as np
 import pandas as pd
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
@@ -8,92 +8,160 @@ from sklearn.preprocessing import LabelEncoder,OrdinalEncoder
 label_encoder = LabelEncoder()
 ordinal_encoder = OrdinalEncoder()
 from encoder import MultiColumnLabelEncoder
+import pickle
 
+tab1, tab2 = st.tabs(["Feature Selection", "Prediction"])
+with tab1:
+    st.header("Home") 
+    st.write("selecting features for prediction!")
 
-def select_k_best_features(data, k=10):
-    X = data.drop(columns=["hv270"])
-    X_final = X[['hv219' ,'hv024','hv025','hv106_01','hv115_01'
-                                  ,'hv201','hv237a','hv237b',"hv009","hv220",
-                                  'hv237c','hv237d','hv237e','hv237f',
-                                  'hv237','hv252','hv230a',"sh121h","sh121j",
-                                  "sh121k","sh121l","sh121m","sh121s","sh121n",
-                                  "sh122h","hv221","hv243e","sh122i","sh121g", 
-                                  "sh121i","sh121p","sh121q","sh121r","hv210",
-                                  "hv243c","hv212","hv243d","hv247"]]
-
-
-
-    #X_final = X_final.replace("don't know", 0)
-    X_final["hv220"]=X_final['hv220'].replace("don't know", 0)
-    #X_final["hv204"]=X_final['hv204'].replace("don't know", 0)
-    #X_final["hv204"]=X_final['hv204'].replace("on premises", 0)
-
-    X_final = X_final.replace("97+", 97)
-    #X_final=X_final.drop("hv204", axis=1)
-    X_final['hv220']= X_final['hv220'].astype('int64')
-
-    #if __name__ == '__main__':
-
-    X_final = MultiColumnLabelEncoder(columns = ['hv219', 'hv024','hv025','hv106_01','hv115_01'
-                                                ,'hv201','hv237a','hv237b', 'hv237c','hv237d',
-                                                'hv237e','hv237f',
-                                  'hv237','hv252','hv230a',"sh121h","sh121j","sh121k",
-                                  "sh121l","sh121m","sh121s","sh121n","sh122h","hv221",
-                                  "hv243e","sh122i","sh121g", "sh121i","sh121p","sh121q",
-                                  "sh121r","hv210","hv243c","hv212","hv243d","hv247"]).fit_transform(X_final)
+    def select_k_best_features(data, k=10):
+        X = data.drop(columns=["hv270"])
+        X_final = X[['hv219' ,'hv024','hv025','hv106_01','hv115_01'
+                                    ,'hv201','hv237a','hv237b',"hv009","hv220",
+                                    'hv237c','hv237d','hv237e','hv237f',
+                                    'hv237','hv252','hv230a',"sh121h","sh121j",
+                                    "sh121k","sh121l","sh121m","sh121s","sh121n",
+                                    "sh122h","hv221","hv243e","sh122i","sh121g", 
+                                    "sh121i","sh121p","sh121q","sh121r","hv210",
+                                    "hv243c","hv212","hv243d","hv247"]]
 
 
 
+        #X_final = X_final.replace("don't know", 0)
+        X_final["hv220"]=X_final['hv220'].replace("don't know", 0)
+        #X_final["hv204"]=X_final['hv204'].replace("don't know", 0)
+        #X_final["hv204"]=X_final['hv204'].replace("on premises", 0)
 
-    y = data[["hv270"]]
-    bestfeatures = SelectKBest(score_func=chi2, k=k)
-    # Initialize SelectKBest with the desired scoring function
-    fit = bestfeatures.fit(X_final,y)
-    dfscores = pd.DataFrame(fit.scores_)
-    dfcolumns = pd.DataFrame(X_final.columns)
-#concat two dataframes for better visualization 
-    featureScores = pd.concat([dfcolumns,dfscores],axis=1)
-    featureScores.columns = ['Specs','Score']  #naming the dataframe columns
-    features_best = featureScores.nlargest(10,'Score') 
-    return features_best #print 10 best features
+        X_final = X_final.replace("97+", 97)
+        #X_final=X_final.drop("hv204", axis=1)
+        X_final['hv220']= X_final['hv220'].astype('int64')
+
+        #if __name__ == '__main__':
+
+        X_final = MultiColumnLabelEncoder(columns = ['hv219', 'hv024','hv025','hv106_01','hv115_01'
+                                                    ,'hv201','hv237a','hv237b', 'hv237c','hv237d',
+                                                    'hv237e','hv237f',
+                                    'hv237','hv252','hv230a',"sh121h","sh121j","sh121k",
+                                    "sh121l","sh121m","sh121s","sh121n","sh122h","hv221",
+                                    "hv243e","sh122i","sh121g", "sh121i","sh121p","sh121q",
+                                    "sh121r","hv210","hv243c","hv212","hv243d","hv247"]).fit_transform(X_final)
 
 
 
 
-def main():
-    st.title('Feature Selection App')
+        y = data[["hv270"]]
+        bestfeatures = SelectKBest(score_func=chi2, k=k)
+        # Initialize SelectKBest with the desired scoring function
+        fit = bestfeatures.fit(X_final,y)
+        dfscores = pd.DataFrame(fit.scores_)
+        dfcolumns = pd.DataFrame(X_final.columns)
+    #concat two dataframes for better visualization 
+        featureScores = pd.concat([dfcolumns,dfscores],axis=1)
+        featureScores.columns = ['Specs','Score']  #naming the dataframe columns
+        features_best = featureScores.nlargest(10,'Score') 
+        return features_best #print 10 best features
 
-    # File uploader
-    uploaded_file = st.file_uploader("Choose a csv file", type="csv")
-    
-    if uploaded_file is not None:
-        # Read the stata file
-        data = pd.read_csv(uploaded_file)
+
+
+
+    def main():
+        st.title('Feature Selection App')
+
+        # File uploader
+        uploaded_file = st.file_uploader("Choose a csv file", type="csv")
         
-        # Display the first few rows of the data
-        st.write("First few rows of the data:")
-        st.write(data.head())
+        if uploaded_file is not None:
+            # Read the stata file
+            data = pd.read_csv(uploaded_file)
+            
+            # Display the first few rows of the data
+            st.write("First few rows of the data:")
+            st.write(data.head())
 
-        # Get the list of columns
-        columns = data.columns.tolist()
+            # Get the list of columns
+            columns = data.columns.tolist()
 
-        # Select target column
-        target_column = st.selectbox("Select the target column", columns)
+            # Select target column
+            target_column = st.selectbox("Select the target column", columns)
 
-        # Select number of features
-        k = st.slider("Select number of features to keep", min_value=1, max_value=len(columns)-1, value=10)
+            # Select number of features
+            k = st.slider("Select number of features to keep", min_value=1, max_value=len(columns)-1, value=10)
 
-        if st.button('Run Feature Selection'):
-            # Run the feature selection
-            selected_data = select_k_best_features(data, k)
-            selected_features = selected_data["Specs"]            
-            # Display the selected features
-            st.write("Selected Features:")
-            st.write(selected_data)
+            if st.button('Run Feature Selection'):
+                # Run the feature selection
+                selected_data = select_k_best_features(data, k)
+                selected_features = selected_data["Specs"]            
+                # Display the selected features
+                st.write("Selected Features:")
+                st.write(selected_data)
 
-            # Display the data with selected features
-            st.write("Data with selected features:")
-            st.write(data[selected_features])
+                # Display the data with selected features
+                st.write("Data with selected features:")
+                st.write(data[selected_features])
 
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
+
+
+with tab2:
+
+    st.header("Prediction")
+    st.write("Predicting the target variable!")
+    with open('model.pkl', 'rb') as file:
+     model = pickle.load(file)
+    mappings = { "Educational level": {"Not educated": 0, "Pre-school": 1, "Primary": 2,"Secondary": 3,"Higher": 4}, 
+                "Marital Status": {"Single": 0, "Married": 1,"Widow":2,"Divorced":3}, 
+                "Region of Origin": {"Far-North": 4, "East": 3,"Centre(Without Yaounde)":1,"Yaoundé":11,"adamawa":0,
+                                     "South-West":10,"South":9,"West":8,"Douala":2,"Littoral(without Douala)":5,
+                                     "North-West":7,"North":6}, 
+                "Residential area": {"Urban": 1, "Rural": 0}, 
+                "Sex": {"Male": 1, "Female": 0} ,
+                "Source of drinking water":{"Tube well or borehole":13,"Pipe to yard or plot":5,"Public tap/ stand pipe":8,"Piped to neighbour":4,
+                                            "Unprotected spring":14, "Protected well":7,"Unprotected well":15,"River/dam/lake/ponds/stream/canal/irrigation channel":10,
+                                            "Protected spring":6,"Bottled water":0,"Piped into dweeling":3,"Cart with small tank":1,"Sachet water":11,
+                                            "Rainwater":9,"Tanker truck":12,"Other":2},
+                "Mixer":{"No":0,"Yes":1},
+                "Gas stove":{"No":0,"Yes":1},
+                "Fan":{"No":0,"Yes":1},
+                "Bank account":{"No":0,"Yes":1},
+                "Laptop/Computer":{"No":0,"Yes":1},
+                "Modem/Router":{"No":0,"Yes":1},
+                "Cooker":{"No":0,"Yes":1},
+                "CD/DVD Player":{"No":0,"Yes":1},
+                "Clock": {"No":0,"Yes":1}
+                }
+    
+    Number_of_Households = st.number_input("Enter the number of households:")
+    Age_of_head_of_household = st.number_input("Enter the age head of households:")
+    # Create a dictionary to store the encoded inputs
+    encoded_inputs = {}
+     # Create a selectbox for each categorical variable
+    for var, mapping in mappings.items(): 
+        user_input = st.selectbox(f"Choose entries for {var}:", list(mapping.keys())) 
+        encoded_inputs[var] = mapping[user_input] 
+        # Display the encoded inputs 
+    #st.write("Encoded inputs:")
+    #st.write(encoded_inputs)
+
+    # Combine encoded categorical variables and continuous variables into a single input array
+    input_data = np.array([encoded_inputs["Educational level"], encoded_inputs["Marital Status"], encoded_inputs["Region of Origin"], 
+                           encoded_inputs["Residential area"], encoded_inputs["Sex"], Number_of_Households, Age_of_head_of_household,
+                           encoded_inputs["Source of drinking water"], encoded_inputs["Mixer"], encoded_inputs["Gas stove"],
+                           encoded_inputs["Fan"], encoded_inputs["Bank account"], encoded_inputs["Laptop/Computer"], 
+                           encoded_inputs["Modem/Router"], encoded_inputs["Cooker"], encoded_inputs["CD/DVD Player"], encoded_inputs["Clock"]
+                           ])
+    #st.write("Input Data:") 
+    #st.write(input_data)
+    if st.button("Predict"):
+        probabilities = model.predict_proba(input_data)
+        # Convert probabilities to percentages
+        percentages = probabilities * 100
+       # Class names
+        class_names = ["Poorest", "Poorer", "Middle", "Richer", "Richest"] 
+        # Display class probabilities using circles (progress bars) 
+        st.write("Class Probabilities:") 
+        for i, percent in enumerate(percentages): 
+            st.write(f"{class_names[i]}: {percent:.2f}%") 
+            st.progress(int(percent))
+        
